142d2a7b877cdb4cd0a930b265d9e98647ce7a0f
jay <jaylinuxmumbai@gmail.com>
2025-06-15 22:03:08 +0530

    15-06-2025 Update

---
+++ day-0-basic/ex-2.py
@@ -0,0 +1,8 @@
+from random import randint
+print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep='')
+
+#ex Libraries of python for ML
+'''Numpy
+   Pandas
+   Matplotlib
+   Sklearn'''
